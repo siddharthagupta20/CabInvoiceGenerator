81a9f6419e3d6d302a3e30a414caab560224facb
siddharthagupta20 <siddharthagupta20@users.noreply.github.com>
2020-10-26 01:07:01 +0530

    [Siddhartha] modified: calculated fare for a ride

--- src/main/java/com/cg/cig1/InvoiceService.java
+++ src/main/java/com/cg/cig1/InvoiceService.java
@@ -1,10 +1,18 @@
 package com.cg.cig1;
 
 public class InvoiceService {
-    public boolean someLibraryMethod() {
-        return true;
-    }
-    public static void main(String [] args) {
+	
+	
+   
+    private static final double COST_PER_KM = 10;
+	private static final int COST_PER_MIN = 1;
+	private static final double MIN_FARE = 5;
+	public static void main(String [] args) {
     	System.out.println("Welcome to cab invoice generator.");
     }
+	public double calcFare(double distance, int time) {
+		double totalFare=COST_PER_KM*distance+COST_PER_MIN*time;
+		return Math.max(MIN_FARE, totalFare);
+		
+	}
 }
