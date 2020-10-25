5d348b57b44f9055950116428fa7c1b729e5ae41
siddharthagupta20 <siddharthagupta20@users.noreply.github.com>
2020-10-26 00:55:56 +0530

    [Siddhartha] created: Cab Invoice Generator

---
+++ src/main/java/com/cg/cig1/InvoiceService.java
@@ -0,0 +1,10 @@
+package com.cg.cig1;
+
+public class InvoiceService {
+    public boolean someLibraryMethod() {
+        return true;
+    }
+    public static void main(String [] args) {
+    	System.out.println("Welcome to cab invoice generator.");
+    }
+}
